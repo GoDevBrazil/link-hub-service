0a99257bf6ce63dd2710cbe820aa97c1623398ac
jecesario <jp@soloid.com.br>
2023-05-20 22:29:18 -0300

    feat(LHUB012): resolving deprecated Jwts methods

--- src/main/java/com/godev/linkhubservice/security/jwt/JwtService.java
+++ src/main/java/com/godev/linkhubservice/security/jwt/JwtService.java
@@ -3,9 +3,10 @@ package com.godev.linkhubservice.security.jwt;
 import com.godev.linkhubservice.properties.JwtProperties;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
@@ -26,18 +27,24 @@ public class JwtService {
         var instant = dateTimeExpiration.atZone(ZoneId.systemDefault()).toInstant();
         var date = from(instant);
 
+        var key = Keys.hmacShaKeyFor(this.jwtProperties.getSignKey().getBytes(StandardCharsets.UTF_8));
+
         return Jwts
                 .builder()
                 .setSubject(email)
                 .setExpiration(date)
-                .signWith(SignatureAlgorithm.HS512, this.jwtProperties.getSignKey())
+                .signWith(key)
                 .compact();
     }
 
     public Claims getClaims(String token) {
+
+        var key = Keys.hmacShaKeyFor(this.jwtProperties.getSignKey().getBytes(StandardCharsets.UTF_8));
+
         return Jwts
-                .parser()
-                .setSigningKey(this.jwtProperties.getSignKey())
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
